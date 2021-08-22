632b23330a4c688c043dcb225a1921babd6548ef
Ghader Ghasemi <68222268+qasemyq@users.noreply.github.com>
2021-08-22 15:35:12 +0200

    Update Navbar.vue

--- Navbar.vue
+++ Navbar.vue
@@ -14,7 +14,7 @@
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        
+         
         <b-navbar-nav>
           <b-nav-item href="#">Saved Property</b-nav-item>
         </b-navbar-nav>
