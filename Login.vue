301a50fc7cf16d11ba30dd2309395061232f6853
Ghader Ghasemi <68222268+qasemyq@users.noreply.github.com>
2021-09-19 18:49:06 +0200

    Update Login.vue

--- Login.vue
+++ Login.vue
@@ -6,7 +6,7 @@
       tabindex="-1"
       role="dialog"
       aria-labelledby="loginTitle"
-      aria-hidden="true"
+      aria-hidden="true" 
     >
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
